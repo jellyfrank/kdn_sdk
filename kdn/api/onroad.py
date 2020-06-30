492a5e51142edb6b70e7a6b24b04f3a32d3a3903
block-cat <kfx2007@outlook.com>
2020-06-30 15:14:51 +0000

    [FIX]在途监控京东、顺丰需要填CustomerName

--- kdn/api/onroad.py
+++ kdn/api/onroad.py
@@ -9,12 +9,13 @@ from .comm import Comm
 
 class Onroad(Comm):
 
-    def get_express_routes(self, order_code, shipper_code, logistic_code):
+    def get_express_routes(self, order_code, shipper_code, logistic_code,customer_name=None):
         """
         即时查询
         order_code: 订单编号
         shipper_code: 快递公司编码
         logistic_code: 快递单号
+        customer_name: 京东、顺丰 必填，京东对应商家编码，顺丰为收件人或发件人手机号后四位
         """
 
         request_type = 8001
@@ -25,7 +26,8 @@ class Onroad(Comm):
         data = {
             "OrderCode": order_code,
             "ShipperCode": shipper_code,
-            "LogisticCode": logistic_code
+            "LogisticCode": logistic_code,
+            "CustomerName": customer_name
         }
 
         return self.post(request_type, data)
